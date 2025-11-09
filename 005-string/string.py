5d44d16b0bc76aa88b4a27af4ac8fdbe18886b25
Javohir TL <Javohir.py@gmail.com>
2025-11-09 13:21:12 +0500

    Strings - Done

--- 005-string/string.py
+++ 005-string/string.py
@@ -21,3 +21,46 @@ print(my_string.count("l"))
 
 other_string = my_string.replace("world", "Univers")  # Agar topolmasa xato chiqmaydi shunchaki skip qiladi.
 print(other_string.title())
+
+my_string = "one, two, three, four, five"
+my_list = my_string.split(", ")
+print(my_list)
+
+new_string = ", ".join(my_list)
+print(new_string)
+
+from timeit import default_timer as timer
+
+#  Bad 
+start = timer()
+my_list = ["O"] * 7000
+# print(my_list)
+my_string = ""
+for item in my_list:
+    my_string += item
+
+# print(my_string)
+end = timer()
+print(f"Bad way took {end - start:2f} seconds")
+
+# Good
+start = timer()
+my_list = ["O" * 7]
+my_string = "".join(my_list)
+# print(my_string)
+end = timer()
+print(f"Good way took {end - start:2f} seconds")
+
+# %, .format(), f-String
+name = "Tomy"
+num = 3.14159265
+print("Hello %d" % num)
+print("Hello %f" % num)
+print("Hello %.8f" % num)
+print("Hello %s" % name)
+
+print("Hello {}, Do you know about {}?".format(name, num))
+print("Hello {}, Do you know about {:.2f}?".format(name, num))
+print("Hello {}, Do you know about {:.8f}? {}".format(name, num, 1648542))
+
+print(f"What's up {name}? Do you know about PI? This is value of the PI {num:.8f}")
