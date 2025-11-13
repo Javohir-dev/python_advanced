d09cbeacd0a6b56eebbd0f248372f787ad1fa11a
Javohir TL <Javohir.py@gmail.com>
2025-11-13 10:08:33 +0500

    if __name__ == __main__:

---
+++ 000-try/index.py
@@ -0,0 +1,7 @@
+from testing import first, second
+
+result1 = first(10, 20)
+result2 = second(50, 30)
+
+print(result1)
+print(result2)
---
+++ 000-try/testing.py
@@ -0,0 +1,14 @@
+def first(x, y):
+    return x + y
+
+def second(x, y):
+    return x - y
+
+
+if __name__ == "__main__":
+    result1 = first(1, 2)
+    result2 = second(5, 3)
+
+    print(result1)
+    print(result2)
+    print(__name__)
